4803f7134d0f4f452f250f56cf98ecf7ca09f643
Samuela31 <94887862+Samuela31@users.noreply.github.com>
2025-03-01 21:52:51 +0530

    Time: 19 ms (23%), Space: 12.3 MB (98.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,13 +4,25 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        #Number Manipulation method
         if x>-1 and x<10:
             return True
 
         if x<0 or x%10==0: #if x is -ve or last digit is 0, then it can't be palindrome. Put this condition after ensuring x is not 0 here
             return False
+
+        #Half Reverse Number Manipulation method (Optimized)
+        res= 0
+        num=x
+        while res < num:
+            res= res*10 + num%10
+            num//= 10
+
+        #res//10 is for number with odd number of digits to remove middle digit
+        return num in (res, res//10) #returns true or false
         
+        '''
+        ##########################################
+        #Number Manipulation method
         num=x
         rem=0
         res=0
@@ -24,7 +36,7 @@ class Solution(object):
             return True
         return False
         
-        '''
+        #########################################
         #Using Strings
         s = str(x)
         if s == s[::-1]:
@@ -32,4 +44,3 @@ class Solution(object):
         else:
             return False
         '''
-        
